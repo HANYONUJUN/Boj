cc7ff086796d1dd34811d095c7f6a1fb962b5432
hanyongjun <gksdydwns34@naver.com>
2023-02-01 01:43:20 +0900

    2576 홀수

---
+++ 2576.py
@@ -0,0 +1,16 @@
+list=[]
+
+for i in range(7):
+    num=int(input())
+    if num % 2 !=0:
+        list.append(num)
+
+if len(list) == 0:
+    print(-1)
+else:
+    print(sum(list))
+    print(min(list))
+
+
+    
+
